0d96104f2ceb962eb5477cfc2e508745437a9293
PranavKudva <panu.kudva09121993@gmail.com>
2015-09-21 00:52:28 +0530

    Implemented remove rules part of rules sync.<Pranav>

--- lib/security/firewall/rule_manager.rb
+++ lib/security/firewall/rule_manager.rb
@@ -30,8 +30,8 @@ module Security
       # },
       # ...
       # ]
-      # done so as to avoid too much network calls to find out apt rule numbers; for a route(from a source zone to a dest zone)
-      # getting rules associated to a route in one place will help in bulk generation of rule numbers
+      # done so as to avoid too much network calls to find out apt hash numbers; for a route(from a source zone to a dest zone)
+      # getting rules associated to a route in one place will help in bulk generation of hash numbers
       private def add_rules(route_grouped_rules)
         for route_grouped_rule in route_grouped_rules do
           src_zone = route_grouped_rule['srcIpZone']
@@ -53,21 +53,38 @@ module Security
         compiled_rules = compile_rules(dependencies, ip_lookup, zone_lookup)
 
         compiled_rules_set = compiled_rules.uniq
-        existing_rules_set = trim_unwanted_keys(get_configured_rules.uniq)
+
+        untrimmed_configured_rules = get_configured_rules
+        existing_rules_set = trim_unwanted_keys(deep_copy(untrimmed_configured_rules).uniq, ["ruleno", "ruletype", "fwCompId", "protocol"])
 
         new_rules_set = compiled_rules_set - existing_rules_set
         obsolete_rules_set = existing_rules_set - compiled_rules_set
 
         add_rules(group_by_route(new_rules_set))
-        # delete_rules(obsolete_rules_set)
+        delete_rules(untrimmed_configured_rules, obsolete_rules_set)
 
         {:added => new_rules_set, :removed => obsolete_rules_set}
       end
 
-      # def delete_rules(unwanted_rules_set)
-      # end
+      private def deep_copy(object)
+        Marshal.load(Marshal.dump(object))
+      end
 
-      # done so that can be consumed by add rule
+      private def delete_rules(untrimmed_configured_rules, obsolete_rules_set)
+        for rule in obsolete_rules_set do
+          rule_matches = untrimmed_configured_rules.select { |config_rule|
+            config_rule["srcIpZone"] == rule["srcIpZone"] && config_rule["destIpZone"] == rule["destIpZone"] &&
+                config_rule["sourceIp"] == rule["sourceIp"] && config_rule["destinationIp"] == rule["destinationIp"] &&
+                config_rule["destPort"] == rule["destPort"]
+          }
+
+          for a_rule in rule_matches do
+            @firewall_client.delete_rule(a_rule["ruleno"], a_rule["srcIpZone"], a_rule["destIpZone"])
+          end
+        end
+      end
+
+      # done so that can be consumed by add hash
       private def group_by_route(rules)
         group_by_fields = ["srcIpZone", 'destIpZone']
 
@@ -75,7 +92,7 @@ module Security
           route_grouped_rule = {
               'srcIpZone' => zones[0],
               'destIpZone' => zones[1],
-              'rules' => route_rules
+              'rules' => trim_unwanted_keys(deep_copy(route_rules), ["srcIpZone", "destIpZone"])
           }
           route_grouped_rules << route_grouped_rule
         end
@@ -86,9 +103,9 @@ module Security
       end
 
       # needed as the compiled rules don't contain the mentioned keys and we don't want it to come up as differentiating factor when we apply '-' operator
-      private def trim_unwanted_keys(existing_rules)
-        for rule in existing_rules do
-          rule.delete_if { |key| ["ruleno", "ruletype", "fwCompId", "protocol"].any? { |unwanted_key| unwanted_key == key } }
+      private def trim_unwanted_keys(hashes, unwanted_keys)
+        for hash in hashes do
+          hash.delete_if { |key| unwanted_keys.any? { |unwanted_key| unwanted_key == key } }
         end
       end
 
@@ -130,7 +147,7 @@ module Security
 
       private def generate_rule_numbers(existing_rules, count)
         existing_rule_numbers = existing_rules.map { |rule| rule["ruleno"] }
-        missing_rule_numbers = (2001..9999).to_a - existing_rule_numbers #rule number between 2001 to 9999 are considered as explicitly added rules set in netmagic
+        missing_rule_numbers = (2001..9999).to_a - existing_rule_numbers #hash number between 2001 to 9999 are considered as explicitly added rules set in netmagic
         missing_rule_numbers[0..count]
       end
 
